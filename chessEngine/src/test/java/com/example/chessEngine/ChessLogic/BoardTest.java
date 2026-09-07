d5363ea1289279edaccb17f566aae564870b1e3e
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-09-06 23:45:45 -0500

    test: add source and destination test

--- chessEngine/src/test/java/com/example/chessEngine/ChessLogic/BoardTest.java
+++ chessEngine/src/test/java/com/example/chessEngine/ChessLogic/BoardTest.java
@@ -5,8 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
 
-  private int ROW = 8;
-  private int COL = 8;
+  private final int ROW = 8;
+  private final int COL = 8;
+  private final int UNDER_BOUND = -1;
+  private final int UPPER_BOUND = 8;
+  private final int VALID_BOUND = 4;
+  private final int DEFAULT_ROW = 3;
+  private final int DEFAULT_COL = 3;
 
   @Test
   public void creationTest() {
@@ -22,6 +27,45 @@ public class BoardTest {
     board.setPiece(0, 0, piece);
 
     assertEquals(piece, board.getPiece(0, 0));
+  }
+
+  @Test
+  public void invalidSourceAndDestinationTest() {
+    Board board = new Board();
+
+    // out of bound test
+    assertFalse(board.verifySourceAndDestination(UNDER_BOUND, VALID_BOUND, DEFAULT_ROW, DEFAULT_COL, true));
+    assertFalse(board.verifySourceAndDestination(UPPER_BOUND, VALID_BOUND, DEFAULT_ROW, DEFAULT_COL, true));
+
+    assertFalse(board.verifySourceAndDestination(VALID_BOUND, UNDER_BOUND, DEFAULT_ROW, DEFAULT_COL, true));
+    assertFalse(board.verifySourceAndDestination(VALID_BOUND, UPPER_BOUND, DEFAULT_ROW, DEFAULT_COL, true));
+
+    assertFalse(board.verifySourceAndDestination(UNDER_BOUND, UNDER_BOUND, DEFAULT_ROW, DEFAULT_COL, true));
+    assertFalse(board.verifySourceAndDestination(UNDER_BOUND, UPPER_BOUND, DEFAULT_ROW, DEFAULT_COL, true));
+
+    assertFalse(board.verifySourceAndDestination(VALID_BOUND, VALID_BOUND, UNDER_BOUND, VALID_BOUND, true));
+    assertFalse(board.verifySourceAndDestination(VALID_BOUND, VALID_BOUND, UPPER_BOUND, VALID_BOUND, true));
+
+    assertFalse(board.verifySourceAndDestination(VALID_BOUND, VALID_BOUND, VALID_BOUND, UNDER_BOUND, true));
+    assertFalse(board.verifySourceAndDestination(VALID_BOUND, VALID_BOUND, VALID_BOUND, UPPER_BOUND, true));
+
+    assertFalse(board.verifySourceAndDestination(VALID_BOUND, VALID_BOUND, VALID_BOUND, UPPER_BOUND, true));
+    assertFalse(board.verifySourceAndDestination(VALID_BOUND, VALID_BOUND, VALID_BOUND, UNDER_BOUND, true));
+
+    // test null cell
+    assertFalse(board.verifySourceAndDestination(VALID_BOUND, VALID_BOUND, VALID_BOUND, VALID_BOUND, true));
+
+    // test correct piece
+    board.setPiece(VALID_BOUND, VALID_BOUND, new Rook(VALID_BOUND, VALID_BOUND, true));
+    assertFalse(board.verifySourceAndDestination(VALID_BOUND, VALID_BOUND, VALID_BOUND, VALID_BOUND, false));
+
+    // test move to valid cell that is either empty or opponent piece
+    board.setPiece(DEFAULT_ROW, DEFAULT_COL, new Pawn(DEFAULT_ROW, DEFAULT_COL, true));
+    assertFalse(board.verifySourceAndDestination(VALID_BOUND, VALID_BOUND, DEFAULT_ROW, DEFAULT_COL, true));
+
+    // test valid move
+    board.setPiece(DEFAULT_ROW, DEFAULT_COL, new Pawn(DEFAULT_ROW, DEFAULT_COL, false));
+    assertTrue(board.verifySourceAndDestination(VALID_BOUND, VALID_BOUND, DEFAULT_ROW, DEFAULT_COL, true));
   }
 
 }
