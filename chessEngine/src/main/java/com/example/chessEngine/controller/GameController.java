d0efc0ef62e1daddca6f6f7e69fa23505a040059
Dat Nguyen <datnguyengithubwork@gmail.com>
2026-09-09 14:55:57 -0500

    fea: add corse policy for new front-end

--- chessEngine/src/main/java/com/example/chessEngine/controller/GameController.java
+++ chessEngine/src/main/java/com/example/chessEngine/controller/GameController.java
@@ -19,8 +19,7 @@ import java.util.UUID;
 
 @RestController
 @CrossOrigin({"http://localhost:5173",
-              "https://flourishing-beijinho-e3811c.netlify.app/",
-              "https://automated-chess-player-1.onrender.com"
+              "https://ai-chess-1df4.onrender.com/"
 })
 public class GameController {
 
